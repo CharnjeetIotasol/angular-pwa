dae2c8f31ee8950084662a7e658bfab6f2d02cb7
Charnjeet Singh <charnjeet.iotasol@gmail.com>
2022-06-16 09:44:42 +0530

    Facebook Login Updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,8 +13,8 @@ export const environment = {
   }),
   GoogleKey: "846338494502-5jhd38d8r57h5j6pgbu17jiik7qpeu7p.apps.googleusercontent.com",
   GoogleSecretKey: "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",
-  FacebookKey: "650995642649107",
-  FacebookSecretKey: "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",
+  FacebookKey: "5107238159372417",
+  FacebookSecretKey: "efaa19be7a475060aaaba935ad9e4716",
 };
 
 /*
