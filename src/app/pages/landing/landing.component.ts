9aab1ac40cf84e08817027463c35394e8e9a907b
Charnjeet Singh <charnjeet.iotasol@gmail.com>
2022-06-17 13:42:45 +0530

    Minor changes

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -23,43 +23,79 @@ import { ToastService } from 'src/app/shared/toast.service';
   ],
 })
 export class LandingComponent implements OnInit {
-  currentTab: string;
   selectedVoucherId: string;
   selectedMarkerId: string;
   selectedTabIndex: number;
   tabView: string;
+  history: Array<string>;
 
   constructor(private loadingService: LoadingService,
     private toastService: ToastService,
     private mapService: MapService) { }
 
   ngOnInit(): void {
+    this.history = new Array<string>();
     this.selectedTabIndex = 0;
-    this.currentTab = "VOCUHER_VIEW";
     this.tabView = "FIND_VOUCHER_VIEW";
+    this.addToHistory(this.tabView);
+    this.toastService.success(this.getPWADisplayMode());
+  }
+
+  getPWADisplayMode() {
+    const isStandalone = window.matchMedia('(display-mode: standalone)').matches;
+    if (document.referrer.startsWith('android-app://')) {
+      return 'twa';
+    } else if (isStandalone) {
+      return 'standalone';
+    }
+    return 'browser';
   }
 
   @HostListener('window:popstate', ['$event'])
   onPopState(event: any) {
-    this.toastService.success('Back button pressed');
+    if (this.history.length <= 1) {
+      console.log("Skipping back button due to no history is available");
+      return;
+    }
+    this.history.pop();
+    const url = this.history[this.history.length - 1];
+    this.tabView = url;
+    if (["FIND_VOUCHER_VIEW", "VOUCHER_COLLECT_VIEW"].indexOf(this.tabView) !== -1) {
+      this.selectedTabIndex = 0;
+    } else if (["MY_VOUCHER_VIEW", "VOUCHER_DETAIL_VIEW"].indexOf(this.tabView) !== -1) {
+      this.selectedTabIndex = 1;
+    } else if (["LEADERBOARD"].indexOf(this.tabView) !== -1) {
+      this.selectedTabIndex = 2;
+    } else if (["PROFILE_VIEW", "CHANGE_PASSWORD_VIEW", "INTEREST_VIEW"].indexOf(this.tabView) !== -1) {
+      this.selectedTabIndex = 3;
+    }
+  }
+
+  addToHistory(url: string) {
+    if (this.history.length > 0 && this.history[this.history.length - 1] === url) {
+      console.log("Skipping because already at last postion");
+      return;
+    }
+    this.history.push(url);
   }
 
   onTabChanged($event: any) {
-    this.currentTab = $event.index === 0 ? "VOCUHER_VIEW"
+    const currentTab = $event.index === 0 ? "VOCUHER_VIEW"
       : $event.index === 1 ? "MY_VOUCHER_VIEW"
         : $event.index === 2 ? "LEADERBOARD_VIEW"
           : $event.index === 3 ? "ACCOUNT_SETTING_VIEW"
             : "OTHER";
 
-    if (this.currentTab === "VOCUHER_VIEW") {
+    if (currentTab === "VOCUHER_VIEW") {
       this.tabView = "FIND_VOUCHER_VIEW";
-    } else if (this.currentTab === "MY_VOUCHER_VIEW") {
+    } else if (currentTab === "MY_VOUCHER_VIEW") {
       this.tabView = "MY_VOUCHER_VIEW";
-    } else if (this.currentTab === "LEADERBOARD_VIEW") {
+    } else if (currentTab === "LEADERBOARD_VIEW") {
       this.tabView = "LEADERBOARD";
-    } else if (this.currentTab === "ACCOUNT_SETTING_VIEW") {
+    } else if (currentTab === "ACCOUNT_SETTING_VIEW") {
       this.tabView = "PROFILE_VIEW";
     }
+    this.addToHistory(this.tabView);
   }
 
 
@@ -67,30 +103,23 @@ export class LandingComponent implements OnInit {
   onCompleteEvent($event: any) {
     if ($event.status === "COMPLETED") {
       this.tabView = "PROFILE_VIEW";
-      return;
     } else if ($event.status === "CHANGE_PASSWORD_REQUESTED") {
       this.tabView = "CHANGE_PASSWORD_VIEW";
-      return;
     } else if ($event.status === "INTEREST_REQUESTED") {
       this.tabView = "INTEREST_VIEW";
-      return;
     } else if ($event.status === "FIND_VOUCHER_REQUESTED") {
-      this.currentTab = "VOCUHER_VIEW";
       this.selectedTabIndex = 0;
       this.tabView = "FIND_VOUCHER_VIEW";
-      return;
     } else if ($event.status === "FIND_VOUCHER_REQUESTED_FROM_MY_VOUCHER") {
-      this.currentTab = "VOCUHER_VIEW";
       this.selectedTabIndex = 0;
       this.tabView = "FIND_VOUCHER_VIEW";
     } else if ($event.status === "VOUCHER_DETAIL_REQUESTED") {
       this.selectedVoucherId = $event.messgae;
       this.tabView = "VOUCHER_DETAIL_VIEW";
-      return;
     } else if ($event.status === "MY_VOUCHER_REQUESTED") {
       this.tabView = "MY_VOUCHER_VIEW";
     }
-
+    this.addToHistory(this.tabView);
   }
 
   onCollectEvent($event: any) {
