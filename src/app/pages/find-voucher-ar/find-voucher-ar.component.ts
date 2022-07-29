393242d28bec82630bc3ab3b2d6a94343b754ff4
Charnjeet Singh <charnjeet.iotasol@gmail.com>
2022-07-29 18:43:00 +0530

    Minor changes

--- src/app/pages/find-voucher-ar/find-voucher-ar.component.ts
+++ src/app/pages/find-voucher-ar/find-voucher-ar.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { environment } from 'src/environments/environment';
 declare const AFRAME: any;
 @Component({
   selector: 'app-find-voucher-ar',
@@ -11,13 +12,25 @@ export class FindVoucherArComponent implements OnInit, AfterViewInit {
   marker: any;
   @Output()
   collectEvent = new EventEmitter<any>();
+  @Output()
+  completeEvent = new EventEmitter<any>();
   hasMarkerDetailOpen: boolean;
   iconPath: string;
   constructor() { }
 
   ngOnInit(): void {
     this.iconPath = this.marker.logo.length > 0 ? this.marker.logo[0].path : '/assets/images/icons/map-pin-default-small.png';
+    if (!environment.production) {
+      this.iconPath = "/assets/images/kfc-logo.png";
+    }
     this.hasMarkerDetailOpen = false;
+  }
+
+
+  back() {
+    setTimeout(() => {
+      this.completeEvent.emit({ "status": "FIND_VOUCHER_REQUESTED", "messgae": "" });
+    });
   }
 
   ngAfterViewInit(): void {
